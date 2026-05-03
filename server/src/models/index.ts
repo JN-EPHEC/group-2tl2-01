3a89a2638375c377d5b1a75a87f3741546d6b07b
teamcharlier <200207490+thomas40404@users.noreply.github.com>
2026-05-03 09:15:27 +0200

    ajout d'un type de cours

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,9 +1,10 @@
 import sequelize from '../config/database';
 import { User } from './User';
 import { Family } from './Family';
+import { CourseType } from './CourseType';
 
-
+// User <-> Family associations
 User.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
 Family.belongsTo(User, { foreignKey: 'responsibleUserId', as: 'responsibleUser' });
 
-export { sequelize, User, Family };
+export { sequelize, User, Family, CourseType };
