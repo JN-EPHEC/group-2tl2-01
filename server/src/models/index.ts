665d1eac1a7249099bf0b28e9f57f21d09559dd5
teamcharlier <200207490+thomas40404@users.noreply.github.com>
2026-05-04 11:37:34 +0200

    Ajout lien famille et membre

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,9 +2,19 @@ import sequelize from '../config/database';
 import { User } from './User';
 import { Family } from './Family';
 import { CourseType } from './CourseType';
+import { Member } from './Member';
+
 
 // User <-> Family associations
 User.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
 Family.belongsTo(User, { foreignKey: 'responsibleUserId', as: 'responsibleUser' });
 
-export { sequelize, User, Family, CourseType };
+// Family <-> Member
+Family.hasMany(Member, { foreignKey: 'familyId', as: 'members' });
+Member.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
+
+export { sequelize, User, Family, CourseType, Member };
+
+
+
+
