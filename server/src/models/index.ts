44c6284a161d6636559799e6d05821a7115f5b19
teamcharlier <200207490+thomas40404@users.noreply.github.com>
2026-05-04 13:43:03 +0200

    lien système de credit

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,7 +3,7 @@ import { User } from './User';
 import { Family } from './Family';
 import { CourseType } from './CourseType';
 import { Member } from './Member';
-
+import { CreditPurchase } from './CreditPurchase';
 
 // User <-> Family associations
 User.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
@@ -13,8 +13,16 @@ Family.belongsTo(User, { foreignKey: 'responsibleUserId', as: 'responsibleUser' 
 Family.hasMany(Member, { foreignKey: 'familyId', as: 'members' });
 Member.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
 
-export { sequelize, User, Family, CourseType, Member };
+// Family <-> CreditPurchase
+Family.hasMany(CreditPurchase, { foreignKey: 'familyId', as: 'creditPurchases' });
+CreditPurchase.belongsTo(Family, { foreignKey: 'familyId', as: 'family' });
 
+// Member <-> CreditPurchase (autonomous)
+Member.hasMany(CreditPurchase, { foreignKey: 'memberId', as: 'creditPurchases' });
+CreditPurchase.belongsTo(Member, { foreignKey: 'memberId', as: 'member' });
 
+// CreditPurchase <-> User (addedBy)
+CreditPurchase.belongsTo(User, { foreignKey: 'addedBy', as: 'addedByUser' });
+User.hasMany(CreditPurchase, { foreignKey: 'addedBy', as: 'addedCredits' });
 
-
+export { sequelize, User, Family, CourseType, Member, CreditPurchase };
